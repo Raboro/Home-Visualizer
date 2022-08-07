5335e0a5589a05e05f4085a19b6a93f5223e2b9d
raboro <raborogit@gmail.com>
2022-08-07 12:40:42 +0200

    isolate LoginActionListener into another file

--- src/HomeVisualizer/Gui/Login/LoginGui.java
+++ src/HomeVisualizer/Gui/Login/LoginGui.java
@@ -1,15 +1,7 @@
 package HomeVisualizer.Gui.Login;
 
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.sql.SQLException;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
@@ -17,23 +9,22 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import HomeVisualizer.Main;
+import HomeVisualizer.ActionListener.LoginActionListener;
 import HomeVisualizer.Gui.Frame;
-import HomeVisualizer.Logic.LoginLogic;
 
 public class LoginGui extends Frame {
-
+    
+    private JPanel panel;
+    private JLabel usernameLabel, passwordLabel;
+    
     private static final String TITLE = "Login";
     private static final int WIDTH = 500;
     private static final int HEIGHT = 500;
 
-    private JPanel panel;
-
-    private JLabel usernameLabel, passwordLabel;
-    private JTextField username;
-    private JPasswordField password;
-    private JButton loginButton, signUpButton;
-    private JCheckBox showPassword;
+    public static JTextField username;
+    public static JPasswordField password;
+    public static JButton loginButton, signUpButton;
+    public static JCheckBox showPassword;
 
     public LoginGui() {
         super(TITLE, WIDTH, HEIGHT);
@@ -42,12 +33,13 @@ public class LoginGui extends Frame {
 
         this.add(panel);
         createLayout();
-        addActionListener();
         this.setLocation(730, 650);
         this.setVisible(true);
     }
 
     private void createLayout() {
+        ActionListener actionListener = new LoginActionListener();
+
         usernameLabel = new JLabel("Username");
         usernameLabel.setBounds(130, 78, 70, 20);
         panel.add(usernameLabel);
@@ -66,81 +58,19 @@ public class LoginGui extends Frame {
 
         showPassword = new JCheckBox("Show Password");
         showPassword.setBounds(130, 200, 200, 20);
+        showPassword.addActionListener(actionListener);
         panel.add(showPassword);
 
         loginButton = new JButton();
         loginButton.setBounds(130, 240, 110, 38);
         loginButton.setIcon(new javax.swing.ImageIcon("/home/marius/Documents/Home-Visualizer/Images/loginButton.png"));
+        loginButton.addActionListener(actionListener);
         panel.add(loginButton);
 
         signUpButton = new JButton();
         signUpButton.setBounds(250, 240, 130, 38);
         signUpButton.setIcon(new javax.swing.ImageIcon("/home/marius/Documents/Home-Visualizer/Images/signUpButton.png"));
+        signUpButton.addActionListener(actionListener);
         panel.add(signUpButton);
-    }
-
-    public void addActionListener() {
-        showPassword.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (showPassword.isSelected()) {
-                    // show each char of password
-                    password.setEchoChar((char) 0);
-                } else {
-                    // hide each char of password 
-                    char c = '*';
-                    password.setEchoChar(c);
-                }
-            }
-
-        });
-
-        loginButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                LoginLogic logic = new LoginLogic(username.getText(), new String(password.getPassword()));
-                boolean loginSuccessful = false;
-
-                try {
-                    loginSuccessful = logic.login();
-                } catch (InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException
-                        | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | SQLException
-                        | IOException e1) {
-                    e1.printStackTrace();
-                }
-
-                String usernameToContinue;
-                if (!loginSuccessful) {
-                    usernameToContinue = null;
-                } else{
-                    usernameToContinue = username.getText();
-                }
-                dispose();
-                continueAfterLogin(usernameToContinue);
-            }
-
-            private void continueAfterLogin(String usernameToContinue) {
-                Main.continueAfterLogin(usernameToContinue);
-            }
-
-        });
-
-        signUpButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                LoginLogic logic = new LoginLogic(username.getText(), new String(password.getPassword()));
-
-                try {
-                    logic.signUp();
-                } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
-                        | InvalidKeySpecException | IllegalBlockSizeException | BadPaddingException | SQLException
-                        | IOException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        });
     }
 }
--- src/HomeVisualizer/Main.java
+++ src/HomeVisualizer/Main.java
@@ -7,13 +7,16 @@ import HomeVisualizer.Gui.Login.LoginGui;
 import HomeVisualizer.Gui.VisualizeMain.StartVisualizeGui;
 
 public class Main {
+
+    private static LoginGui login;
+
     public static void main(String[] args) throws SQLException {
         new InitializeDatabase();
-        new LoginGui();
+        login = new LoginGui();
     }
 
-    
     public static void continueAfterLogin(String username) {
+        login.dispose();
         if (!(username == null)) {
             new StartVisualizeGui(username);
         }
