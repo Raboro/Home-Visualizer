7391820a0c114e67f6e066003191a70f4b8686e3
Raboro <raborogit@gmail.com>
2022-08-15 22:30:20 +0200

    update bin & continue step 3 for four walls

--- src/HomeVisualizer/Graphics/CreateRoomNamesGraphics.java
+++ src/HomeVisualizer/Graphics/CreateRoomNamesGraphics.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 
 import HomeVisualizer.Gui.VisualizeMain.StepsGui.CreateApartmentGui;
 import HomeVisualizer.Logic.StartVisualizerStepsLogic.CreateApartmentLogic;
+import HomeVisualizer.Logic.StartVisualizerStepsLogic.CreateRoomNamesLogic;
 
 import java.awt.Color;
 import java.awt.Graphics;
@@ -23,12 +24,12 @@ public class CreateRoomNamesGraphics extends JFrame {
     }
 
     public void paint(Graphics g) {
-        g.setFont(new Font("Arial",Font.BOLD,12)); 
-        g.drawString("This is Draw Line Example", 100, 70);   
-        g.setColor(Color.blue);
-        // g.drawLine(90, 135, 90, 180); 
-        // g.setColor(Color.green); 
-        // g.drawLine(60, 4, 120, 120); 
+        g.setFont(new Font("Arial", Font.BOLD, 12));
+        g.drawString("This is Draw Line Example", 100, 70);
+        g.setColor(Color.BLACK);
+        // g.drawLine(90, 135, 90, 180);
+        // g.setColor(Color.green);
+        // g.drawLine(60, 4, 120, 120);
 
         // g.drawLine(10, 10, 100, 10);
         // g.drawLine(10, 10, 10, 100);
@@ -36,28 +37,23 @@ public class CreateRoomNamesGraphics extends JFrame {
         // g.drawLine(100, 10, 100, 100);
 
         if (CreateApartmentLogic.userChooseFourWalls) {
-            double length = (Integer.parseInt(CreateApartmentGui.getApartmentParameter[0].getText()) * 2) / 10;
-            double width = (Integer.parseInt(CreateApartmentGui.getApartmentParameter[2].getText()) * 2) / 10;
+            int length = (int) (Integer.parseInt(CreateApartmentGui.getApartmentParameter[0].getText()) * 2) / 10;
+            int width = (int) (Integer.parseInt(CreateApartmentGui.getApartmentParameter[2].getText()) * 2) / 10;
 
-            if ((length >= 1000 || width >= 1000) ) {
-                length /= 5;
-                width /= 5;
+            int[] size = CreateRoomNamesLogic.formatSizeParameter(length, width);
+            length = size[0];
+            width = size[1];
+
+            g.drawLine(100, 100, 100, (100 + width));
+            g.drawLine(100, 100, (100 + length), 100);
+            g.drawLine(100, (100 + width), (100 + length), (100 + width));
+            g.drawLine((100 + length), 100, (100 + length), (100 + width));
+
+            int[][] walls = CreateRoomNamesLogic.formatSizeParameterWalls();
+
+            for (int[] wall: walls) {
+                g.drawLine(wall[0], wall[1], wall[2], wall[3]);
             }
-
-            if (length >= 800 || width >= 800) {
-                length /= 4;
-                width /= 4;
-            }
-
-            if (length >= 600 || width >= 600) {
-                length /= 2;
-                width /= 2;
-            }
-
-            g.drawLine(100, 100, 100, (int) (100 + width));
-            g.drawLine(100, 100, (int) (100 + length), 100);
-            g.drawLine(100, (int) (100 + width), (int) (100 + length), (int) (100 + width));
-            g.drawLine((int) (100 + length), 100, (int) (100 + length), (int) (100 + width));
 
 
         } else {
--- src/HomeVisualizer/Logic/StartVisualizerStepsLogic/CreateRoomNamesLogic.java
+++ src/HomeVisualizer/Logic/StartVisualizerStepsLogic/CreateRoomNamesLogic.java
@@ -5,7 +5,10 @@ import javax.swing.SwingUtilities;
 import HomeVisualizer.Graphics.CreateRoomNamesGraphics;
 
 public class CreateRoomNamesLogic {
-    
+
+    private static int divideFactor;
+    private static int[][] wallPoints = new int[CreateRoomsLogic.wallPoints.size()][4];
+
     public static void init() {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -14,5 +17,57 @@ public class CreateRoomNamesLogic {
                 g.setVisible(true);
             }
         });
+    }
+
+    public static int[] formatSizeParameter(int length, int width) {
+        if (divideFactor > 0) {
+            if ((length >= 1000 || width >= 1000)) {
+                length /= 5;
+                width /= 5;
+            }
+    
+            if (length >= 800 || width >= 800) {
+                length /= 4;
+                width /= 4;
+            }
+    
+            if (length >= 600 || width >= 600) {
+                length /= 2;
+                width /= 2;
+            }
+        }
+        if ((length >= 1000 || width >= 1000)) {
+            length /= 5;
+            width /= 5;
+            divideFactor += 5;
+        }
+
+        if (length >= 800 || width >= 800) {
+            length /= 4;
+            width /= 4;
+            divideFactor += 4;
+        }
+
+        if (length >= 600 || width >= 600) {
+            length /= 2;
+            width /= 2;
+            divideFactor += 2;
+        }
+        return new int[] { length, width };
+    }
+
+    public static int[][] formatSizeParameterWalls() {
+        for (int wall = 0; wall < CreateRoomsLogic.wallPoints.size(); wall++) {
+            for (int parameter = 0; parameter < 4; parameter++) {
+                wallPoints[wall][parameter] = (int) CreateRoomsLogic.wallPoints.get(wall)[parameter] / 10;
+
+                if (wallPoints[wall][parameter] < 0) {
+                    wallPoints[wall][parameter] *= -1;
+                } 
+                wallPoints[wall][parameter] /= divideFactor;
+                wallPoints[wall][parameter] += 100;
+            }
+        }
+        return wallPoints;
     }
 }
