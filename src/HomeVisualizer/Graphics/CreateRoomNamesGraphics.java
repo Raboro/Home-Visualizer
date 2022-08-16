8f47f76bc73612c3c28cfe84ad76f65c771261bf
Raboro <raborogit@gmail.com>
2022-08-16 23:03:12 +0200

    four walls work; !4 in work; step 3

--- src/HomeVisualizer/Graphics/CreateRoomNamesGraphics.java
+++ src/HomeVisualizer/Graphics/CreateRoomNamesGraphics.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import HomeVisualizer.Gui.VisualizeMain.StepsGui.CreateApartmentGui;
 import HomeVisualizer.Logic.StartVisualizerStepsLogic.CreateApartmentLogic;
 import HomeVisualizer.Logic.StartVisualizerStepsLogic.CreateRoomNamesLogic;
+import HomeVisualizer.Logic.StartVisualizerStepsLogic.CreateRoomsLogic;
 
 import java.awt.Color;
 import java.awt.Graphics;
@@ -14,6 +15,7 @@ public class CreateRoomNamesGraphics extends JFrame {
 
     private static final int HEIGHT = 1000;
     private static final int WIDTH = 1700;
+    private static final int METER_INTO_PIXEL = 2; 
 
     public CreateRoomNamesGraphics() {
         super("Show Apartment");
@@ -37,10 +39,10 @@ public class CreateRoomNamesGraphics extends JFrame {
         // g.drawLine(100, 10, 100, 100);
 
         if (CreateApartmentLogic.userChooseFourWalls) {
-            int length = (int) (Integer.parseInt(CreateApartmentGui.getApartmentParameter[0].getText()) * 2) / 10;
-            int width = (int) (Integer.parseInt(CreateApartmentGui.getApartmentParameter[2].getText()) * 2) / 10;
+            int length = (int) (Integer.parseInt(CreateApartmentGui.getApartmentParameter[0].getText()) * METER_INTO_PIXEL) / 10;
+            int width = (int) (Integer.parseInt(CreateApartmentGui.getApartmentParameter[2].getText()) * METER_INTO_PIXEL) / 10;
 
-            int[] size = CreateRoomNamesLogic.formatSizeParameter(length, width);
+            int[] size = CreateRoomNamesLogic.formatSizeParameter(new int[]{length, width});
             length = size[0];
             width = size[1];
 
@@ -49,15 +51,27 @@ public class CreateRoomNamesGraphics extends JFrame {
             g.drawLine(100, (100 + width), (100 + length), (100 + width));
             g.drawLine((100 + length), 100, (100 + length), (100 + width));
 
-            int[][] walls = CreateRoomNamesLogic.formatSizeParameterWalls();
-
-            for (int[] wall: walls) {
-                g.drawLine(wall[0], wall[1], wall[2], wall[3]);
-            }
-
+            
 
         } else {
+            // don´t work, need to save the divideFactor
+            for (double[] wallAsDouble : CreateRoomsLogic.wallPoints) {
+                int[] wall = new int[4];
 
+                for (int i = 0; i < wallAsDouble.length; i++) {
+                    wall[i] = (int) (wallAsDouble[i] * METER_INTO_PIXEL) / 10;
+                }
+                
+                wall = CreateRoomNamesLogic.formatSizeParameter(wall);
+                System.out.println(wall[0] + ", " + wall[1] + ", "+ wall[2] + ", " + wall[3]);
+                g.drawLine(wall[0], wall[1], wall[2], wall[3]);
+            }
+        }
+
+        int[][] walls = CreateRoomNamesLogic.formatSizeParameterWalls();
+
+        for (int[] wall: walls) {
+            g.drawLine(wall[0], wall[1], wall[2], wall[3]);
         }
     }
 }
--- src/HomeVisualizer/Logic/StartVisualizerStepsLogic/CreateRoomNamesLogic.java
+++ src/HomeVisualizer/Logic/StartVisualizerStepsLogic/CreateRoomNamesLogic.java
@@ -7,6 +7,8 @@ import HomeVisualizer.Graphics.CreateRoomNamesGraphics;
 public class CreateRoomNamesLogic {
 
     private static int divideFactor;
+    private static int[] checkValue = { 1000, 800, 600 };
+    private static int[] decreaseValueBy = { 5, 4, 2 };
     private static int[][] wallPoints = new int[CreateRoomsLogic.wallPoints.size()][4];
 
     public static void init() {
@@ -19,43 +21,37 @@ public class CreateRoomNamesLogic {
         });
     }
 
-    public static int[] formatSizeParameter(int length, int width) {
-        if (divideFactor > 0) {
-            if ((length >= 1000 || width >= 1000)) {
-                length /= 5;
-                width /= 5;
-            }
-    
-            if (length >= 800 || width >= 800) {
-                length /= 4;
-                width /= 4;
-            }
-    
-            if (length >= 600 || width >= 600) {
-                length /= 2;
-                width /= 2;
-            }
+    private static int[] translateValues(int[] values, int check) {
+        for (int i = 0; i < values.length; i++) {
+            values[i] /= decreaseValueBy[check];
         }
-        if ((length >= 1000 || width >= 1000)) {
-            length /= 5;
-            width /= 5;
-            divideFactor += 5;
-        }
-
-        if (length >= 800 || width >= 800) {
-            length /= 4;
-            width /= 4;
-            divideFactor += 4;
-        }
-
-        if (length >= 600 || width >= 600) {
-            length /= 2;
-            width /= 2;
-            divideFactor += 2;
-        }
-        return new int[] { length, width };
+        return values;
     }
 
+    public static int[] formatSizeParameter(int[] values) {
+        for (int value = 0; value < values.length; value++) {
+            if (values[value] < 0) {
+                values[value] *= -1;
+            }
+
+            for (int check = 0; check < checkValue.length; check++) {
+                if (values[value] >= checkValue[check]) {
+                    if (divideFactor == 0) {
+                        divideFactor += decreaseValueBy[check];
+                    } else {
+                        divideFactor = divideFactor <= decreaseValueBy[check] ? decreaseValueBy[check] : divideFactor;
+                    }
+                    translateValues(values, check);
+                }
+            }
+
+            if (values[value] == 0) {
+                values[value] = 100;
+            } 
+        }
+        return values;
+    }
+    
     public static int[][] formatSizeParameterWalls() {
         for (int wall = 0; wall < CreateRoomsLogic.wallPoints.size(); wall++) {
             for (int parameter = 0; parameter < 4; parameter++) {
@@ -63,7 +59,7 @@ public class CreateRoomNamesLogic {
 
                 if (wallPoints[wall][parameter] < 0) {
                     wallPoints[wall][parameter] *= -1;
-                } 
+                }
                 wallPoints[wall][parameter] /= divideFactor;
                 wallPoints[wall][parameter] += 100;
             }
