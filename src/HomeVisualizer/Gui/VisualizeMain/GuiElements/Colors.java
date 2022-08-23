9680e2008af18585d4c93ce64e3e2a92d87e5ac0
Raboro <raborogit@gmail.com>
2022-08-23 14:10:14 +0200

    refactor: remove unnecassary lines

--- src/HomeVisualizer/Gui/VisualizeMain/GuiElements/Colors.java
+++ src/HomeVisualizer/Gui/VisualizeMain/GuiElements/Colors.java
@@ -3,10 +3,9 @@ package HomeVisualizer.Gui.VisualizeMain.GuiElements;
 import java.awt.Color;
 
 public class Colors {
-    
+
     public static final Color START_STEPS_COLOR = new Color(46, 134, 193);
     public static final Color START_STEPS_COLOR_BORDER = new Color(46, 134, 255);
-
     public static final Color UNFULFILLED_STEP_COLOR = new Color(238, 75, 43);
     public static final Color UNFULFILLED_STEP_COLOR_BORDER = new Color(255, 105, 43);
     public static final Color IN_WORK_STEP_COLOR = new Color(241, 196, 15);
