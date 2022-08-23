3194b2dbe2507b69b40ed86c6f05274296ccfff6
Raboro <raborogit@gmail.com>
2022-08-23 13:58:12 +0200

    refactor: split one method into multiple

--- src/HomeVisualizer/Gui/Login/LoginGui.java
+++ src/HomeVisualizer/Gui/Login/LoginGui.java
@@ -39,39 +39,51 @@ public class LoginGui extends Frame {
     }
 
     private void createLayout() {
-        ActionListener actionListener = new LoginActionListener();
+        initializeLabels();
+        initializeInputFields();
+        initializeInteractions(new LoginActionListener());
+    }
 
+    private void initializeLabels() {
         usernameLabel = new JLabel("Username");
-        usernameLabel.setBounds(130, 78, 70, 20);
-        panel.add(usernameLabel);
-
-        username = new JTextField();
-        username.setBounds(130, 97, 250, 28);
-        panel.add(username);
-
         passwordLabel = new JLabel("Password");
+
+        usernameLabel.setBounds(130, 78, 70, 20);
         passwordLabel.setBounds(130, 135, 70, 20);
+
+        panel.add(usernameLabel);
         panel.add(passwordLabel);
+    }
 
+    private void initializeInputFields() {
+        username = new JTextField();
         password = new JPasswordField();
+
+        username.setBounds(130, 97, 250, 28);
         password.setBounds(130, 155, 250, 28);
-        panel.add(password);
 
+        panel.add(username);
+        panel.add(password);   
+    }
+
+    private void initializeInteractions(ActionListener actionListener) {
         showPassword = new JCheckBox("Show Password");
-        showPassword.setBounds(130, 200, 200, 20);
-        showPassword.addActionListener(actionListener);
-        panel.add(showPassword);
-
         loginButton = new JButton();
-        loginButton.setBounds(130, 240, 110, 38);
-        loginButton.setIcon(new javax.swing.ImageIcon("/home/marius/Documents/Home-Visualizer/Images/loginButton.png"));
-        loginButton.addActionListener(actionListener);
-        panel.add(loginButton);
-
         signUpButton = new JButton();
+
+        showPassword.setBounds(130, 200, 200, 20);
+        loginButton.setBounds(130, 240, 110, 38);
         signUpButton.setBounds(250, 240, 130, 38);
+
+        loginButton.setIcon(new javax.swing.ImageIcon("/home/marius/Documents/Home-Visualizer/Images/loginButton.png"));
         signUpButton.setIcon(new javax.swing.ImageIcon("/home/marius/Documents/Home-Visualizer/Images/signUpButton.png"));
+
+        showPassword.addActionListener(actionListener);
+        loginButton.addActionListener(actionListener);
         signUpButton.addActionListener(actionListener);
+
+        panel.add(showPassword);
+        panel.add(loginButton);
         panel.add(signUpButton);
     }
 }
