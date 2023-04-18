899861ddbab62fc7eeb66263a489821de28e0a9f
Raboro <marius.woerfelreal@gmail.com>
2023-04-18 23:17:44 +0200

    Revert "fixed transform"
    This reverts commit ad5fa5ab90843ab7a8c9471951f26b523db878bc.

--- Frontend/jest.config.ts
+++ Frontend/jest.config.ts
@@ -173,9 +173,9 @@ export default {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    "^.+\\.js$": "babel-jest"
-  },
+  transform: [
+    "^.+\\.js$"
+  ],
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
