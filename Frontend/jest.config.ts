ca936d816eb58b3ea8e5764a218dfe742b097303
Raboro <marius.woerfelreal@gmail.com>
2023-04-18 22:57:30 +0200

    edited jest config

--- Frontend/jest.config.ts
+++ Frontend/jest.config.ts
@@ -8,7 +8,7 @@ export default {
   // automock: false,
 
   // Stop running tests after `n` failures
-  // bail: 0,
+  bail: 1,
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "C:\\Users\\Studium\\AppData\\Local\\Temp\\jest",
@@ -185,7 +185,7 @@ export default {
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  verbose: true,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
